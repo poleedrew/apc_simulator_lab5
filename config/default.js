b4086f49937b5f6bd49832266ce9679c20e32193
pikachu04230 <leo0009859@gmail.com>
2022-06-06 23:59:56 +0800

    Modify config for GKE

--- config/default.js
+++ config/default.js
@@ -6,10 +6,12 @@ module.exports = {
   mongodb: {
     db: 'controlParameter',
     collection: 'factor',
-    url: 'mongodb://localhost:27017/',
+    // url: 'mongodb://localhost:27017/',
+    url: 'mongodb://mongodb/',
   },
   nats: {
-    connection: '127.0.0.1:4222',
+    // connection: '127.0.0.1:4222',
+    connection: 'nats-server',
     name: 'testbed',
     stream: 'testbed_stream',
     subject: 'testbed.subject',
